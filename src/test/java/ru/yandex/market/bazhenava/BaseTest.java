a2348d41b7859a34669f94a21d5178232c388631
Aksana Bazhenava <bazhenava1.17@gmail.com>
2024-09-20 03:22:35 +0300

    refactor classes BaseTest, HomePage.                  refactor pom.xml

--- src/test/java/ru/yandex/market/bazhenava/BaseTest.java
+++ src/test/java/ru/yandex/market/bazhenava/BaseTest.java
@@ -1,4 +1,24 @@
 package ru.yandex.market.bazhenava;
 
+import ru.yandex.market.bazhenava.pages.HomePageXPath;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseTest {
+
+    public WebDriver driver;
+
+    @BeforeEach
+    public void driverSetUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(HomePageXPath.BASE_URL);
+    }
+
+    @AfterEach
+    public void driverQuit() {
+        driver.quit();
+    }
 }
