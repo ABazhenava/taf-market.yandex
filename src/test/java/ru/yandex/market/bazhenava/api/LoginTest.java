0613e34cb820f6cbf1ec0710976974e2f571a49a
Aksana Bazhenava <bazhenava1.17@gmail.com>
2024-09-25 13:22:22 +0300

    add class LoginTest, delete class LoginInputCorrectAndIncorrectDateTest

---
+++ src/test/java/ru/yandex/market/bazhenava/api/LoginTest.java
@@ -0,0 +1,58 @@
+package ru.yandex.market.bazhenava.api;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class LoginTest {
+
+    @Test
+    public void testGetResponseStatusCode500() {
+        RestAssured
+                .when()
+                .get("https://mail.yandex.ru/api/v2/userid/counters?silent")
+                .then()
+                .statusCode(500).log().body();
+    }
+
+    @Test
+    public void testGetLogin() {
+        RestAssured
+                .when()
+                .get("https://api.passport.yandex.ru/registration_status/check")
+                .then()
+                .statusCode(200)
+                .body("is_completion_recommended", equalTo(false))
+                .body("is_completion_required", equalTo(false));
+    }
+
+    @Test
+    public void testPostLogin() {
+        RestAssured
+                .when()
+                .post("https://passport.yandex.ru/registration-validations/auth/accounts")
+                .then()
+                .statusCode(200)
+                .body("accounts.loginForLogoId", equalTo(null))
+                .body("accounts.isQRAuthEnabled", equalTo(true));
+    }
+
+    @Test
+    public void testPostLogin403() {
+        RestAssured
+                .when()
+                .post("https://passport.yandex.ru/registration-validations/auth/multi_step/start")
+                .then()
+                .statusCode(403);
+    }
+
+    @Test
+    public void testPostLogin2() {
+        RestAssured
+                .when()
+                .post("https://passport.yandex.ru/registration-validations/auth/multi_step/commit_password")
+                .then()
+                .statusCode(403);
+    }
+}
