e9562a8dba458798823267f6c4739e5663812192
Aksana Bazhenava <bazhenava1.17@gmail.com>
2024-09-25 12:45:47 +0300

    add class User

---
+++ src/main/java/ru/yandex/market/bazhenava/user/User.java
@@ -0,0 +1,64 @@
+package ru.yandex.market.bazhenava.user;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.yandex.market.bazhenava.driver.DriverSingleton;
+import ru.yandex.market.bazhenava.pages.login.LoginPageXPath;
+import ru.yandex.market.bazhenava.utils.Waiters;
+
+import java.time.Duration;
+
+public class User {
+    WebDriver driver;
+    private Faker faker;
+    WebDriverWait webDriverWait;
+
+    public User() {
+        this.driver = DriverSingleton.getDriver();
+        faker = new Faker();
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(3));
+    }
+
+    public void inputRandomEmail() {
+        By inputEmailBy = By.xpath(LoginPageXPath.INPUT_EMAIL_XPATH);
+        WebElement inputEmail = driver.findElement(inputEmailBy);
+        Waiters.waitFor(2);
+        inputEmail.sendKeys(faker.internet().emailAddress());
+        Waiters.waitFor(4);
+    }
+
+    public void inputCorrectEmail() {
+        By inputEmailBy = By.xpath(LoginPageXPath.INPUT_EMAIL_XPATH);
+        WebElement inputEmail = driver.findElement(inputEmailBy);
+        inputEmail.sendKeys(LoginPageXPath.CORRECT_EMAIL);
+        Waiters.waitFor(4);
+    }
+
+    public void inputRandomPassword() {
+        By inputPasswordBy = By.xpath(LoginPageXPath.INPUT_PASSWORD_XPATH);
+        WebElement inputPassword = driver.findElement(inputPasswordBy);
+        String randomPassword = faker.numerify("######");
+        Waiters.waitFor(4);
+        inputPassword.sendKeys(randomPassword);
+        Waiters.waitFor(2);
+    }
+
+    public void inputCorrectPassword() {
+        By inputPasswordBy = By.xpath(LoginPageXPath.INPUT_PASSWORD_XPATH);
+        WebElement inputPassword = driver.findElement(inputPasswordBy);
+        inputPassword.sendKeys(LoginPageXPath.CORRECT_PASSWORD);
+        Waiters.waitFor(4);
+    }
+
+
+
+
+
+
+
+
+}
