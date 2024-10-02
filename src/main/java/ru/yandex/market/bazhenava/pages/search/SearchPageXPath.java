95a754c2fa17a5b56d3e28c7d7fc82b2863f0388
Aksana Bazhenava <bazhenava1.17@gmail.com>
2024-10-02 05:05:54 +0300

    refactor classes SearchPageXPath

--- src/main/java/ru/yandex/market/bazhenava/pages/search/SearchPageXPath.java
+++ src/main/java/ru/yandex/market/bazhenava/pages/search/SearchPageXPath.java
@@ -4,5 +4,5 @@ public class SearchPageXPath {
 
     public static final String SEARCH_FIELD_FOR_INPUT = "//input[@id='header-search']";
     public static final String SEARCH_BUTTON_START_ENTER = "//div[@data-auto='search-row']/form/div/button/span";
-    public static final String SEARCH_RESULT_SAMSUNG = "Смартфон Samsung Galaxy S24 Ultra 12 ГБ/1 ТБ Titanium Green";
+    public static final String SEARCH_RESULT_SAMSUNG = "Смартфон Samsung Galaxy S24 Ultra 12 ГБ/1 ТБ Titanium Gray";
 }
